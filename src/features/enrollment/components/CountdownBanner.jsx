fce55565164075837a8637f54d48c115fed6fdad
sebastianboari <luciosebastianboari@gmail.com>
2025-11-17 15:43:08 -0300

    fix: countdown banner - last day message

--- src/features/enrollment/components/CountdownBanner.jsx
+++ src/features/enrollment/components/CountdownBanner.jsx
@@ -1,6 +1,16 @@
 const CountdownBanner = ({ daysRemaining }) => {
   const plural = daysRemaining === 1 ? 'día' : 'días'
   
+  if(daysRemaining == 0){
+    return (
+      <header className='bg-primary/10 dark:bg-primary/20 w-full absolute top-0 left-0'>
+          <div className='container mx-auto px-4 py-2'>
+              <p className='text-center text-sm font-medium text-primary'>Último día para seleccionar tu grupo.</p>
+          </div>
+      </header>
+    )
+  }
+
   return (  
     <header className='bg-primary/10 dark:bg-primary/20 w-full absolute top-0 left-0'>
         <div className='container mx-auto px-4 py-2'>
